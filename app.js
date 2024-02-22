7222a259f892c3c35fc9664f679364fe54f9f7c6
rita <ritaastorga10@gmail.com>
2024-02-21 22:48:47 -0300

    finalicé la lógica del eventListener

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const cronometro = document.getElementById("cronometro");
 const botonInicioPausa = document.getElementById("botonInicioPausa");
-const botonReiniciar = document.getElementById("botonReiciar");
+const botonReiniciar = document.getElementById("botonReiniciar");
 
-let [hora, minutos, segundos] = [0, 0, 0];
+let [horas, minutos, segundos] = [0, 0, 0];
 
 let intervaloDeTiempo;
 let estadoCronometro = "pausado";
@@ -35,11 +35,36 @@ botonInicioPausa.addEventListener("click", function () {
   if (estadoCronometro === "pausado") {
     intervaloDeTiempo = window.setInterval(actualizarCronometro, 1000);
     botonInicioPausa.innerHTML = '<i class="bi bi-pause-fill"></i>';
-    botonInicioPausa.classList.remove("pausar");
+    botonInicioPausa.classList.remove("iniciar");
+    botonInicioPausa.classList.add("pausar");
     estadoCronometro = "corriendo";
   } else {
     window.clearInterval(intervaloDeTiempo);
     botonInicioPausa.innerHTML = '<i class="bi bi-play-fill"></i>';
     botonInicioPausa.classList.remove("pausar");
+    botonInicioPausa.classList.add("iniciar");
+    estadoCronometro = "pausado";
   }
+});
+
+botonReiniciar.addEventListener("click", function () {
+  window.clearInterval(intervaloDeTiempo);
+
+  horas = 0;
+  minutos = 0;
+  segundos = 0;
+
+  //Reiniciar
+
+  cronometro.innerText = "00:00:00";
+
+  // Actualizar botones
+
+  botonInicioPausa.innerHTML = '<i class="bi bi-play-fill"></i>';
+  botonInicioPausa.classList.remove("pausar");
+  botonInicioPausa.classList.add("iniciar");
+
+  // Estado
+
+  estadoCronometro = "pausado";
 });
